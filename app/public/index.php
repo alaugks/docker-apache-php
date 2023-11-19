caa79102a95888ea2f554504ef3a7f95ed055752
André Laugks <al@pb82.de>
2023-11-19 17:00:20 +0100

    Add improvements

--- app/public/index.php
+++ app/public/index.php
@@ -1,4 +1,9 @@
 <?php
-$a = 0;
-$b = 0;
-phpinfo();
+
+use App\Kernel;
+
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
---
+++ app/tests/DummyTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class DummyTest extends KernelTestCase
+{
+    public function testDummy()
+    {
+        $this->assertTrue(true);
+    }
+}
